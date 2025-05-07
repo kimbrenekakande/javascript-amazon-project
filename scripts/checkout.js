9d20ca55fa56a301bd8aff98b4dc3acb27762d8c
Kakande <kimbrenekakande@gmail.com>
2025-05-07 13:15:51 +0300

    object oriented programming

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import "../data/cart-oop.js";
 
 // Call the function to render the order summary
 renderOrderSummary();
