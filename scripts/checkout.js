31187bd933cf9edc08cdd7de47e4cbc8a92f5e69
Kimbrene Kakande <kakande@Kimbrenes-iMac.local>
2025-05-22 18:13:26 +0300

    final

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,19 @@ import { renderOrderSummary } from "./checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import "../data/cart-oop.js";
 
+
+async function loadPage(){
+  console.log('load Page');
+  return 'fucked mokers'
+}
+
+loadPage().then((value) => {
+    console.log('Page loaded');
+    console.log(value);
+})
+
+
+
 // Wait for DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', () => {
     // Load products and then render the components
